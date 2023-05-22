81b225834cc70cf4b1e24bdaca3ea47db8465cba
vaishrachit <vaishrachit87@gmail.com>
2023-05-22 14:38:27 +0530

    code commit

--- src/main/java/com/ideathon/breedingservice/video/Message.java
+++ src/main/java/com/ideathon/breedingservice/video/Message.java
@@ -15,40 +15,5 @@ public class Message {
 	private String receiverName;
 	private String message;
 	private String date;
-    
-    
-    
-    
-	public Message(String senderName, String receiverName, String message, String date) {
-		super();
-		this.senderName = senderName;
-		this.receiverName = receiverName;
-		this.message = message;
-		this.date = date;
-	}
-	public String getSenderName() {
-		return senderName;
-	}
-	public void setSenderName(String senderName) {
-		this.senderName = senderName;
-	}
-	public String getReceiverName() {
-		return receiverName;
-	}
-	public void setReceiverName(String receiverName) {
-		this.receiverName = receiverName;
-	}
-	public String getMessage() {
-		return message;
-	}
-	public void setMessage(String message) {
-		this.message = message;
-	}
-	public String getDate() {
-		return date;
-	}
-	public void setDate(String date) {
-		this.date = date;
-	}
-	    
+
 }
